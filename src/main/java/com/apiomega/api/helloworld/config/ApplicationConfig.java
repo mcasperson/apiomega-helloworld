8e0d6549f3bf51d78b877d6af4d94baa5d95a94e
Matthew Casperson <matthewcasperson@gmail.com>
2016-02-28 14:43:24 +1000

    Fixed up an error where Elide classes were being added as API endpoints

--- src/main/java/com/apiomega/api/helloworld/config/ApplicationConfig.java
+++ src/main/java/com/apiomega/api/helloworld/config/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package com.apiomega.api.helloworld.config;
 
+import com.apiomega.api.helloworld.api.Api;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * There are a few ways to bootstrap a JAX-RS application. See
@@ -10,5 +14,10 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("/")
 public class ApplicationConfig extends Application {
-
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<>();
+        classes.add(Api.class);
+        return classes;
+    }
 }
